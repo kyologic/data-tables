2845516c3a83f719d37323d4fe4bf5f0c91df580
solardog1982 <jzhess88@gmail.com>
2022-04-15 16:24:26 -0400

    Added DataTables Library; Built pull of data from FM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //Here we're importing items we'll need. You can add other imports here.
-
 var table;
 
 // exposing loadData to FileMaker Script
@@ -8,18 +7,18 @@ window.loadData = function (json) {
   //TODO: Get data from FileMaker
   //TODO: prep it for the JS.
 
-  //TODO: create the columns.
+const obj = JSON.parse(json);
+const {columns, data, options} = obj;
+
+console.log(columns);
 
   //TODO: Explore options of Datatables
   //https://datatables.net/reference/option/
   //https://datatables.net/examples/index
   // Create the DataTable, after destroying it if already exists
   table = $("#dtable").DataTable({
-    paging: true,
-    pageLength: 20,
-    searching: true,
-    colReorder: true,
-    columns: [],
-    data: [],
+    options,
+    columns: columns,
+  data: data,
   });
 };
